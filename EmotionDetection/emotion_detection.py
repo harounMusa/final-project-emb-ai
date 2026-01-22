d9a4bfdd059a5743b488742c1453686b48a9028b
harounMusa <haroun.m.nasreldin@gmail.com>
2026-01-22 03:30:19 -0500

    Package the application

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,31 @@
+import requests
+import json
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    payload = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    response_dict = json.loads(response.text)
+    if response.status_code == 200:
+        emotions = response_dict['emotionPredictions'][0]['emotion']
+        score = 0
+        dominant_emotion = max(emotions, key=emotions.get)
+        
+        res = {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion
+        }
+        return res
+
+    return {'message': 'error'}
